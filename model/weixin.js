2d03898081bcf75fc57e08673d0a668508ef1bae
youngdeer <young_deer@sina.com>
2016-05-30 20:43:56 +0800

    Signed-off-by: youngdeer <young_deer@sina.com>

--- model/weixin.js
+++ model/weixin.js
@@ -129,7 +129,7 @@ function sendEventMsg(){
         }else{
             context = totalCost.toString();
         }
-        sendTotalCost(context);
+        Weixin.sendTotalCost(context);
     });
 }
 
